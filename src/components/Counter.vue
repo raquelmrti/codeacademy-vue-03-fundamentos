4318f86a5d9326315fca9df7520ffa8e49fc35c3
Raquel Martínez <raquel.mrti@gmail.com>
2023-04-08 01:56:28 +0200

    add computed property 'squareCounted'

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,12 @@
 <template>
 <h2>Counter</h2>
 <p> {{ counter }} <sup>2</sup></p>
+<p> {{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+<p> {{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+<p> {{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+<p> {{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+<p> {{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+<p> {{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
 
 </template>
 
@@ -12,6 +18,18 @@ export default {
       counter: 5
     }
   },
+  methods: {
+    getSquareValue() {
+      console.log("getSquareValue")
+      return this.counter * this.counter
+    }
+  },
+  computed: {
+    squareCounter() {
+      console.log("computed squareCounter")
+      return this.counter * this.counter
+    }
+  }
 }
 </script>
 
